f965f950d9fe94bf6890f005eac329817460f0b8
Javier Urbina Rathgeb <urbinarathgeb@gmail.com>
2026-02-26 01:08:14 -0300

    fix import admin in users.controllers.js

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -1,4 +1,4 @@
-import { admin } from '../config/firebase.js';
+import  admin  from '../config/firebase.js';
 import { pool } from '../db.js';
 
 // Nota: Gracias al middleware, req.user ya contiene los datos de PostgreSQL.
